5b49c5a760d90def965903a6db9e0d6dd865a115
Crude-005 <chiragsugla@gmail.com>
2024-07-13 14:03:44 +0530

    changed href

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ export default function Navbar({ title = "NavBar", homeText = "Home", mode, togg
   return (
     <nav className={`navbar navbar-expand-lg navbar-${mode ? "dark" : "light"} bg-${mode ? "dark" : "light"}`}>
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {title}
         </a>
         <button
@@ -21,7 +21,7 @@ export default function Navbar({ title = "NavBar", homeText = "Home", mode, togg
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 {homeText}
               </a>
             </li>
